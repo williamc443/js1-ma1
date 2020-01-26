a3b8f9face157edfebcb3ba4357eeba8d8f37bd2
williamc443 <57712080+williamc443@users.noreply.github.com>
2020-01-26 23:34:43 +0100

    js1-ma1

---
+++ William-cunningham-js1-ma1.js
@@ -0,0 +1,84 @@
+// Question 1
+
+var cat = { complain: "Meow!" };
+
+// Question 2
+
+document.querySelector("h3");
+
+var heading = <h3>Subheading</h3>;
+
+// Question 3
+
+// document.querySelectorAll(".h3").object.style.textEmphasis = "em";
+
+const heading1 = document.querySelector("h1");
+
+heading1.style.style.padding = "2em";
+
+// Question 4
+
+document.querySelector(".heading").classList.add("subHeading");
+console.dir(listItems);
+
+// Question 5
+
+document.querySelector("p");
+{
+  var paragraphs;
+}
+
+// Question 6
+
+<div class="results"></div>;
+
+const resultsContainer = document.querySelector(".results");
+console.dir(resultsContainer).classList;
+
+document.querySelector(".results").innerHTML = "New paragraph";
+
+// Question 7
+
+function array(catArray) {
+  for (let i = 0; i < catArray.length; i++) {
+    console.dir(catArray.name[i]);
+  }
+}
+const CATS = catArray;
+[
+  ({
+    name: "Blob",
+    age: 10
+  },
+  {
+    name: "Harold"
+  },
+  {
+    name: "Blurt",
+    age: 21
+  })
+];
+array();
+
+// Question 8
+
+function array(catArray) {
+  var cats;
+  for (let i = 0; i < catArray.length; i++) {
+    console.dir(catArray.name[i]);
+    return cats;
+  }
+}
+const cats = [
+  {
+    name: "<h5>Blob</h5>",
+    age: 10
+  },
+  {
+    name: "<h5>Harold</h5>"
+  },
+  {
+    name: "<h5>Blurt</h5>",
+    age: 21
+  }
+];
